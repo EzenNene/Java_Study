2d74eb0ba9840e46f9024e9ff4db6018559ce300
EzenNene <kkhj0822@gmail.com>
2022-11-28 18:41:26 +0900

    1128

--- src/CharacterMaking/CharacterMakingEx.java
+++ src/CharacterMaking/CharacterMakingEx.java
@@ -4,24 +4,24 @@ import java.util.Scanner;
 
 public class CharacterMakingEx {
 
-	public static final String RESET = "\u001B[0m";    
-	public static final String FONT_BLACK = "\u001B[30m";    
-	public static final String FONT_RED = "\u001B[31m";     
-	public static final String FONT_GREEN = "\u001B[32m";    
-	public static final String FONT_YELLOW = "\u001B[33m";     
-	public static final String FONT_BLUE = "\u001B[34m";   
-	public static final String FONT_PURPLE = "\u001B[35m";     
-	public static final String FONT_CYAN = "\u001B[36m";    
-	public static final String FONT_WHITE = "\u001B[37m";     
-	public static final String BACKGROUND_BLACK = "\u001B[40m";     
-	public static final String BACKGROUND_RED = "\u001B[41m";     
-	public static final String BACKGROUND_GREEN = "\u001B[42m";    
-	public static final String BACKGROUND_YELLOW = "\u001B[43m";     
-	public static final String BACKGROUND_BLUE = "\u001B[44m";    
-	public static final String BACKGROUND_PURPLE = "\u001B[45m";     
-	public static final String BACKGROUND_CYAN = "\u001B[46m";     
+	public static final String RESET = "\u001B[0m";
+	public static final String FONT_BLACK = "\u001B[30m";
+	public static final String FONT_RED = "\u001B[31m";
+	public static final String FONT_GREEN = "\u001B[32m";
+	public static final String FONT_YELLOW = "\u001B[33m";
+	public static final String FONT_BLUE = "\u001B[34m";
+	public static final String FONT_PURPLE = "\u001B[35m";
+	public static final String FONT_CYAN = "\u001B[36m";
+	public static final String FONT_WHITE = "\u001B[37m";
+	public static final String BACKGROUND_BLACK = "\u001B[40m";
+	public static final String BACKGROUND_RED = "\u001B[41m";
+	public static final String BACKGROUND_GREEN = "\u001B[42m";
+	public static final String BACKGROUND_YELLOW = "\u001B[43m";
+	public static final String BACKGROUND_BLUE = "\u001B[44m";
+	public static final String BACKGROUND_PURPLE = "\u001B[45m";
+	public static final String BACKGROUND_CYAN = "\u001B[46m";
 	public static final String BACKGROUND_WHITE = "\u001B[47m";
-	
+
 //	private static ArrayList<Character> characterList = new ArrayList<Character>();
 
 	// 캐릭터 생성해서 넣어놓을 배열 선언
@@ -59,20 +59,23 @@ public class CharacterMakingEx {
 		 */
 
 		while (monster.isAlive()) {
-			
+
 			boolean noCharactersAlive = true;
-			
+
 			for (Character characterList : characterList) {
 				if (characterList.isAlive()) {
+					// 캐릭터 배열을 돌리면서 한명이라도 살아있으면 [살아있는 캐릭터는 없다] 변수에 false 대입
 					noCharactersAlive = false;
 				}
 			}
-			
+
 			if (noCharactersAlive) {
+				// 만약 모든 반복에서 하나라도 위의 if문 조건을 충족하지 못하면
+				// (살아있는 캐릭터가 없다가 true 이면) 그대로 while문 종료
 				break;
 			}
 
-			System.out.println("<"+ turn++ + "번째 턴>" + "\n");
+			System.out.println("<" + turn++ + "번째 턴>" + "\n");
 			// 모든 캐릭터로부터 한번씩 공격받기 (characterList 사용)
 			for (Character character : characterList) {
 				if (monster.isAlive() && character.isAlive()) {
@@ -88,41 +91,34 @@ public class CharacterMakingEx {
 						monster.attack(character);
 					}
 				}
-				
+
 				System.out.println("===========================");
-				
+
 			}
 		} // while 문 끝===========================
 
+		// while 반복문이 끝났는데 몬스터가 살아있으면 게임오버 출력
+		// while 반복문이 끝났는데 몬스터가 살아있지않으면 몬스터 사망 출력
 		if (monster.isAlive()) {
 			System.out.println("게임오버");
-		} else {			
+		} else {
 			System.out.println("============================");
 			System.err.println("몬스터가 사망했습니다.");
 		}
 
 	} // 메인함수 끝 ==============================================================
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	private static void makeCharacter() {
 
 		int characterCount = 0;
 
+		// 캐릭터가 3 미만일 때 while문 (캐릭터 만드는 것) 반복
 		while (characterCount < 3) {
 
 			Scanner scanner = new Scanner(System.in);
 
-			System.out.println(BACKGROUND_GREEN + "====== " + (characterCount + 1) 
-					+ " 번째 캐릭터 이름을 정하세요 ======" + "\n" + RESET);
+			System.out.println(
+					BACKGROUND_GREEN + "====== " + (characterCount + 1) + " 번째 캐릭터 이름을 정하세요 ======" + "\n" + RESET);
 			String chaName = scanner.nextLine();
 			System.out.println("====== 캐릭터 직업 선택 ======");
 			System.out.println(FONT_YELLOW + "1: 전사 2: 마법사 3: 도적" + RESET);
@@ -155,7 +151,8 @@ public class CharacterMakingEx {
 			}
 
 			characterCount++;
-		}
+
+		} // while 문 끝 ============================================================
 
 	}
 }
